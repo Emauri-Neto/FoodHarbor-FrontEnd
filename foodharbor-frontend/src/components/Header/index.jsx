39a163421eea6370aa23a2fe180042bb8cb00ab7
Gabriel <gabrielfsoares0706@gmail.com>
2023-12-28 10:00:05 -0300

    changes from the old project

--- foodharbor-frontend/src/components/Header/index.jsx
+++ foodharbor-frontend/src/components/Header/index.jsx
@@ -1,11 +1,59 @@
 import React from "react";
 
+import StyledLink from "../../styles/StyledStyledLink";
+
+import LogoImg from "../../assets/Logo-FoodHarbor.png";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMagnifyingGlass,
+  faLocationDot,
+  faUser,
+  faBagShopping,
+} from "@fortawesome/free-solid-svg-icons";
+
+import { ContainerHeader } from "./style";
+
 const Header = () => {
-    return(
-        <>
-            <h1>header</h1>
-        </>
-    )
-}
+  return (
+    <ContainerHeader>
+      <StyledLink to="/">
+        <img src={LogoImg} alt="Logo FoodHarbor" />
+      </StyledLink>
+
+      <label className="label-search">
+        <input
+          type="text"
+          placeholder="Buscar um restaurante..."
+          id="input-search"
+        />
+        <button className="search">
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            style={{ marginRight: 3 }}
+          />
+          Buscar
+        </button>
+      </label>
+      <label className="wrapper-buttons">
+        <button className="button-address">
+          <FontAwesomeIcon icon={faLocationDot} style={{ marginRight: 3 }} />
+          Entrega:
+        </button>
+        <select name="user" className="button-user">
+          <option value="">
+            <FontAwesomeIcon icon={faUser} style={{ marginRight: "3px" }} />
+            Usuario
+          </option>
+          <option value="test">Teste</option>
+        </select>
+        <button className="button-purchase">
+          <FontAwesomeIcon icon={faBagShopping} style={{ marginRight: 3 }} />
+          Sacola
+        </button>
+      </label>
+    </ContainerHeader>
+  );
+};
 
 export default Header;
---
+++ foodharbor-frontend/src/components/Navbar/index.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+import pizza from "./img/pizzaIcon.png";
+import lanche from "./img/snackIcon.png";
+import basket from "./img/basketIcon.png";
+import drinks from "./img/drinksIcon.png";
+import fastfood from "./img/fastfoodIcon.png";
+import cake from "./img/cakeIcon.svg";
+import sushi from "./img/sushiIcon.png";
+import bread from "./img/breadIcon.png";
+
+import StyledLink from "../../styles/StyledStyledLink";
+
+import { ContainerNav, LinkNav } from "./style";
+
+const Navbar = () => {
+  return (
+    <ContainerNav>
+      <LinkNav to="/">
+        <span>
+          <img src={pizza} alt="" />
+          <p>Pizza</p>
+        </span>
+      </LinkNav>
+      <LinkNav to="/">
+        <span>
+          <img src={lanche} alt="" />
+          <p>Lanches</p>
+        </span>
+      </LinkNav>
+      <LinkNav to="/">
+        <span>
+          <img src={basket} alt="" />
+          <p>Mercado</p>
+        </span>
+      </LinkNav>
+
+      <LinkNav to="/">
+        <span>
+          <img src={drinks} alt="" />
+          <p>Bebidas</p>
+        </span>
+      </LinkNav>
+      <LinkNav to="/">
+        <span>
+          <img src={fastfood} alt="" className="fastFoodImg" />
+          <p>Fast Food</p>
+        </span>
+      </LinkNav>
+
+      <LinkNav to="/">
+        <span>
+          <img src={cake} alt="" />
+          <p>Doces</p>
+        </span>
+      </LinkNav>
+      <LinkNav to="/">
+        <span>
+          <img src={sushi} alt="" />
+          <p>Sushi</p>
+        </span>
+      </LinkNav>
+
+      <LinkNav to="/">
+        <span>
+          <img src={bread} alt="" />
+          <p>Padaria</p>
+        </span>
+      </LinkNav>
+    </ContainerNav>
+  );
+};
+
+export default Navbar;
